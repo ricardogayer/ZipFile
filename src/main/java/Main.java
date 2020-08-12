cac9fb23ef62faf8c779d4416d6bd84f7ba28d53
rrgayer <zujpyP-wapxuk-ficgy4>
2020-08-12 20:41:05 -0300

    uuid p/ senha  commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,9 +4,12 @@ import net.lingala.zip4j.model.ZipParameters;
 import net.lingala.zip4j.model.enums.AesKeyStrength;
 import net.lingala.zip4j.model.enums.EncryptionMethod;
 
+import java.io.File;
+import java.util.UUID;
+
 public class Main {
 
-    public static void main(String[] args) throws ZipException {
+    public static void main(String[] args) throws ZipException, InterruptedException {
 
         /* https://github.com/srikanth-lingala/zip4j */
 
@@ -20,16 +23,32 @@ public class Main {
 
         /* Zip com senha */
 
+        UUID uuid = UUID.randomUUID();
+
+        System.out.println("UUID = " + uuid.toString());
+
         ZipParameters zipParameters = new ZipParameters();
         zipParameters.setEncryptFiles(true);
         zipParameters.setEncryptionMethod(EncryptionMethod.AES);
         zipParameters.setAesKeyStrength(AesKeyStrength.KEY_STRENGTH_256);
         zipParameters.setFileComment("Ricardo Gayer");
-        ZipFile zipFile = new ZipFile("arquivo.zip", "minhasenha".toCharArray());
+        ZipFile zipFile = new ZipFile("arquivo.zip", uuid.toString().toCharArray());
         zipFile.addFile("arquivo.html", zipParameters);
 
+        Thread.sleep(5000);
+
+        File file = new File("arquivo.html");
+
+        if (file.delete()) {
+            System.out.println("Arquivo excluído com sucesso");
+        } else {
+            System.out.println("Erro durante a exclusão do arquivo");
+        }
+
+        Thread.sleep(5000);
+
         /* Unzip com senha */
-        new ZipFile("arquivo.zip","minhasenha".toCharArray()).extractAll(".");
+        new ZipFile("arquivo.zip",uuid.toString().toCharArray()).extractAll(".");
 
     }
 
